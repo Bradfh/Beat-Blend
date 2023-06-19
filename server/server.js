d5400f90c64f3fb013ba6338bbe4636e4183082a
Bradfh <128874401+Bradfh@users.noreply.github.com>
2023-06-19 13:55:05 -0500

    minor changes

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const cors = require("cors");
 
-const PORT = process.env.PORT || 3001;
+const PORT = Number.parseInt(process.env.PORT) || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
